224f4d7c5c649bd54c9ec360d0b15b15ee790e42
hoffmannvinicius <vinicius.hoffmann.oliveira@hotmail.com>
2023-05-21 21:53:16 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('contato/', views.contato),
-    path('sobre/', views.sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,12 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Vinicius Hoffmann'})
-
-
-def contato(request):
-    return render(request, 'temp/contato.html')
-
-
-def sobre(request):
-    return render(request)
+    return render(request, 'recipes/home.html', context={'name': 'Vinicius'})
